20869e325809010687511c4b40b6d8211f457ad9
Anthony Hurtado <anhurtado@outlook.com>
2024-09-23 11:51:18 -0400

    feat: added swagger configuration

--- src/app/common/health/controllers/health.controller.ts
+++ src/app/common/health/controllers/health.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { HealthService } from './../services/health.service';
 import { Routing } from '@src/core/models/routing.model';
 import { HttpResponse } from '@src/core/models/http-response.model';
 
+@ApiTags('health')
 @Controller(`${Routing.COMMON}/health`)
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
+  @ApiOperation({ summary: 'Health Check' })
   @Get()
   getHealth(): HttpResponse {
     const data = this.healthService.getHealth();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,46 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Validations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Serialization in the whole app
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Doctiver API')
+    .setDescription('Doctiver Project API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // Start the server
+  await app.listen(process.env.NODE_PORT || 3000);
 }
 bootstrap();
