6639146647270b75175667c574b5a5b59f057b36
Anthony Hurtado <anhurtado@outlook.com>
2024-09-23 17:15:59 -0400

    feat: added i18n integration

--- src/app/common/health/services/health.service.ts
+++ src/app/common/health/services/health.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class HealthService {
+  constructor(private readonly i18nService: I18nService) {}
+
   getHealth(): string {
-    return 'It works!';
+    return this.i18nService.t('common.works');
   }
 }
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,5 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import * as path from 'path';
 
 import config from '@src/core/configs/config';
 import { environment } from './configs/environment';
@@ -14,7 +16,16 @@ import joiSchema from './configs/joi-schema';
       load: [config],
       validationSchema: joiSchema,
     }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      logging: true,
+      loaderOptions: {
+        path: path.join(__dirname, '../../i18n'),
+        watch: true,
+      },
+      resolvers: [new HeaderResolver(['lang'])],
+    }),
   ],
-  exports: [ConfigModule],
+  exports: [ConfigModule, I18nModule],
 })
 export class CoreModule {}
